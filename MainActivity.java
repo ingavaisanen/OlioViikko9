cc1cc5beeef3372e85d12780488050951577669b
ingavaisanen <128291687+ingavaisanen@users.noreply.github.com>
2023-03-27 20:28:41 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.userprogram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        UserStorage u = UserStorage.getInstance();
+
+    }
+
+    public void switchToAddUser(View view) {
+        Intent intent = new Intent(this, AddUserActivity.class);
+        startActivity(intent);
+    }
+}
